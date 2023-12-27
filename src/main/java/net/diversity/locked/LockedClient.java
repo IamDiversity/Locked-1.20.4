e3f1c44b67e2885a4e92a1c190c035df73ffd4e2
Diversity <karim.1030@live.fr>
2023-12-27 03:00:04 +0100

    Initial commit

---
+++ src/main/java/net/diversity/locked/LockedClient.java
@@ -0,0 +1,12 @@
+package net.diversity.locked;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LockedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
---
+++ src/main/java/net/diversity/locked/LockedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.diversity.locked;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LockedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
